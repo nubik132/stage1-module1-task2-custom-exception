ee9f9d541f29e2cd6247aa0068b689f6f86e9511
nubik132 <mister.nubik@list.ru>
2023-11-29 12:18:06 +0300

    fix: formatting

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,23 +3,23 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    Student student = Student.getValueOf(studentID);
-    if (student == null){
-      throw new StudentNotFoundException("Could not find student with ID " + studentID);
-    }
-    return student;
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) {
+        Student student = Student.getValueOf(studentID);
+        if (student == null) {
+            throw new StudentNotFoundException("Could not find student with ID " + studentID);
+        }
+        return student;
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+
+        for (int i = 0; i < IDs.length; i++) {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+        }
+
+    }
 }
